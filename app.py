5a1d8968c823af958dca5f0d62d0585de530753f
Gregory Rash <gregoryrash@Gregorys-MacBook-Pro.local>
2020-05-16 00:40:57 -0500

    add minor updates

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
+# Import dependencies
 import numpy as np
 import datetime as dt
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
 from flask import Flask, jsonify
 
 #################################################
